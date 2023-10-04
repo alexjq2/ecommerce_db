bc5d45d98a6e4fbdcfdb422172e27081f50cb4d5
alex <alexqcanchan@gmail.com>
2023-10-04 11:49:27 -0500

    a

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -1,6 +1,8 @@
 const catchError = require('../utils/catchError');
 const Cart = require('../models/Cart');
 const Product = require('../models/Product');
+const Image = require('../models/Image');
+
 
 const getAll = catchError(async (req, res) => {
     const {id} = req.user
