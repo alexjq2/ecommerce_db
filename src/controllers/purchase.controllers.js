af79c2418126c4d25090a3d62fa5360f465f0d58
alex <alexqcanchan@gmail.com>
2023-10-04 12:00:51 -0500

    a

--- src/controllers/purchase.controllers.js
+++ src/controllers/purchase.controllers.js
@@ -1,12 +1,16 @@
 const catchError = require('../utils/catchError');
 const Purchase = require('../models/Purchase');
 const Cart = require('../models/Cart');
+const Image = require('../models/Image');
 
 const getAll = catchError(async (req, res) => {
     const {id} = req.user
     const purchase = await Purchase.findAll({
-        include: [Product],
-        where: {userId: id}
+        where: {userId: id},
+        include: [{
+            model: Product,
+            include: [Image]
+        }],
     });
     return res.json(purchase);
 });
